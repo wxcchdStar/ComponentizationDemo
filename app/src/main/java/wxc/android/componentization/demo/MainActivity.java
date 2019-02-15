c4e8dd381d9e8c95417f97bdd5187ac675d96f3b
chenhaidong <chenhaidong1@xiaomi.com>
2019-02-15 17:09:40 +0800

    init project

---
+++ app/src/main/java/wxc/android/componentization/demo/MainActivity.java
@@ -0,0 +1,14 @@
+package wxc.android.componentization.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
